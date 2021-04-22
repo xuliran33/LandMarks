c668ad4e6faeb8c0b2844811ab8a890ff80ab129
os_xuliran@admin <xuliran123@126.com>
2021-04-22 16:00:34 +0800

    Initial Commit

---
+++ LandMarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LandMarks
+//
+//  Created by os_xuliran on 2021/4/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LandMarks/LandMarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandMarksApp.swift
+//  LandMarks
+//
+//  Created by os_xuliran on 2021/4/22.
+//
+
+import SwiftUI
+
+@main
+struct LandMarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
